e2adaf7804585500f66375d2f0502f6965e35151
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2021-01-22 15:46:35 +0000

    color picker react app completed

---
+++ src/Palette.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/styles";
+import ColorBox from "./ColorBox";
+import NavBar from "./NavBar";
+import PaletteFooter from "./PaletteFooter";
+import styles from "./styles/PaletteStyles.js";
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: "hex",
+      level: 500,
+    };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+  render() {
+    const { colors, paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        showingFullPalette={true}
+        moreUrl={`/palette/${id}/${color.id}`}
+      />
+    ));
+    return (
+      <div className={classes.Palette}>
+        <NavBar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          showingAllColors
+        />
+
+        <div className={classes.Colors}>{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Palette);
---
+++ src/PaletteFormNav.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import PaletteMetaForm from "./PaletteMetaForm";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Button from "@material-ui/core/Button";
+import styles from "./styles/PaletteFormNavStyles";
+
+class PaletteFormNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newPaletteName: "",
+      formShowing: false,
+    };
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+  }
+  showForm() {
+    this.setState({ formShowing: true });
+  }
+  hideForm() {
+    this.setState({ formShowing: false });
+  }
+  render() {
+    const {
+      classes,
+      open,
+      handleSubmit,
+      palettes,
+      handleDrawerOpen,
+    } = this.props;
+    const { formShowing } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          color="default"
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <ChevronRightIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              Create a Palette
+            </Typography>
+          </Toolbar>
+          <div className={classes.navBtns}>
+            <Link to="/" className={classes.link}>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+              >
+                Go Back
+              </Button>
+            </Link>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={this.showForm}
+            >
+              Save Palette
+            </Button>
+          </div>
+        </AppBar>
+        {formShowing && (
+          <PaletteMetaForm
+            handleSubmit={handleSubmit}
+            palettes={palettes}
+            hideForm={this.hideForm}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
